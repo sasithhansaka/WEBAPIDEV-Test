e5049825d39af4a51067a5c0898f7986f17a2dcc
sasithhansaka <sasithhansaka285@gmail.com>
2026-07-05 10:17:46 +0530

    Fix formatting issues in pings route handler

--- routes/pings.js
+++ routes/pings.js
@@ -4,11 +4,7 @@ import { seedData } from "../db.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-<<<<<<< HEAD
     res.status(200).json(seedData.pings);
-=======
-  res.status(200).json(seedData.pings);
->>>>>>> be134a919c578c0b6824d7eb18c2bb520ce56d40
 });
 
 export default router;
